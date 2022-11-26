e3916dd4baf9e202bd7c5d9a21ea2fa499073d1b
Noam Maoz <maoznoam91@gmail.com>
2022-11-26 19:05:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,7 +35,6 @@ let currentTime = new Date();
 displayDate.innerHTML = formatDate(currentTime);
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "215576bab28022db35e6e64f040e1b56";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
